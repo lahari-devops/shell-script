5fe2376411f5aae5de9bc3742cb6d026ab1565a2
sajja surya <sajja.rao@zeiss.com>
2025-08-06 17:57:50 +0530

    deleted

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-MOVIES="pushpa" "rrr" "devara"
+MOVIES=("pushpa" "rrr" "devara")
 
 echo "First movie: ${MOVIES[0]}"
 echo "Second movie: ${MOVIES[1]}"
 echo "Third movie: ${MOVIES[2]}"
+echo "All movies are: ${MOVIES[@]}"
