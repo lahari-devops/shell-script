c54663c1cbb31c10c444c3a4b21d63f06406e592
sajja surya <sajja.rao@zeiss.com>
2025-08-06 17:31:11 +0530

    adding numbers

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -3,22 +3,5 @@
 NUMBER1=$1
 NUMBER2=$2
 
-TIMESTAMP=$(date)
-echo "Script executed at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
-
-echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
-
-#Addition of 2 numbers
-
-# N1=$1
-# N2=$2
-
-# NUM=$(($N1+$N2))
-# echo " adding 2 numbers = $NUM "
-
-# NUMBER1=$1
-# NUMBER2=$2
-
-# SUM=$(($NUMBER1+$NUMBER2))
-# echo "Sum of 2 numbers: $SUM"
+echo "Sum of two numbers: $SUM"
