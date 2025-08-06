2337e7e05b82fb3285a4afd5b7d00563027f48ac
sajja surya <sajja.rao@zeiss.com>
2025-08-06 17:22:39 +0530

    adding numbers

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -2,8 +2,14 @@
 
 #Addition of 2 numbers
 
-N1=$1
-N2=$2
+# N1=$1
+# N2=$2
 
-NUM=$(($N1+$N2))
-echo " adding 2 numbers = $NUM "
+# NUM=$(($N1+$N2))
+# echo " adding 2 numbers = $NUM "
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+echo "Sum of 2 numbers: $SUM"
