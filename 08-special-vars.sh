c1c798d5c1ddc3882eea4ef1be78459e130dee3c
sajja surya <sajja.rao@zeiss.com>
2025-08-08 07:14:58 +0530

    movies

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -10,5 +10,6 @@ echo "Present working directory is : $PWD"
 echo "Which user is running this script : $USER"
 echo "Home directory of current user : $HOME"
 echo "Process id of current script is : $$"
+sleep 100 &
 echo "process id of previous command running in background is : $!"
 echo "Exit status of previous command is : $?"
