6d8f5fce619c81b39fe6cb2faac45358ddcafceb
sajja surya <sajja.rao@zeiss.com>
2025-08-08 07:12:24 +0530

    movies

---
+++ 08-special-vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUM1=$1
+NUM2=$2
+NUM3=$3
+
+echo "Number of variables passed : $#"
+echo "All the variables passed : $@"
+echo "Present working directory is : $PWD"
+echo "Which user is running this script : $USER"
+echo "Home directory of current user : $HOME"
+echo "Process id of current script is : $$"
+echo "process id of previous command running in background is : $!"
+echo "Exit status of previous command is : $?"
