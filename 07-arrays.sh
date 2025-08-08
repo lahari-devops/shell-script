b3712b1c5ed4217528f3542fe9ca5195756456dd
sajja surya <sajja.rao@zeiss.com>
2025-08-08 06:59:50 +0530

    movies

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,7 +2,7 @@
 
 MOVIES=("pushpa" "rrr" "devara")
 
-echo "First movie: ${MOVIES[0]}"
-echo "Second movie: ${MOVIES[1]}"
-echo "Third movie: ${MOVIES[2]}"
-echo "All movies are: ${MOVIES[@]}"
+echo "First movie : ${MOVIES[0]}"
+echo "Second movie : ${MOVIES[1]}"
+echo "Third movie : ${MOVIES[2]}"
+echo "All movies are : ${MOVIES[@]}"
