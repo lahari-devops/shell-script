a06932b43659a16fe5b693718cc4685000076604
sajja surya <sajja.rao@zeiss.com>
2025-08-26 06:01:07 +0530

    some message

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-MOVIES=("$1" "$2" "$3")
+# MOVIES=("$1" "$2" "$3")
 
-echo "First movie : ${MOVIES[0]}"
-echo "Second movie : ${MOVIES[1]}"
-echo "Third movie : ${MOVIES[2]}"
-echo "All movies are : ${MOVIES[@]}"
+# echo "First movie : ${MOVIES[0]}"
+# echo "Second movie : ${MOVIES[1]}"
+# echo "Third movie : ${MOVIES[2]}"
+# echo "All movies are : ${MOVIES[@]}"
+
+
+MOVIES=("svsc" "fida" "nvnv")
+
+echo "First movie is ${MOVIES[0]}"
+echo "Second movie is ${MOVIES[1]}"
+echo "Third movie is ${MOVIES[2]}"
