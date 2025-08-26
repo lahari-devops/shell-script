4e2ccba51b84e29d4638f4ff520f59f8fe72de96
sajja surya <sajja.rao@zeiss.com>
2025-08-26 05:36:57 +0530

    some message

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-VAR1=jerry
-VAR2=tom
+# VAR1=jerry
+# VAR2=tom
+
+# echo "$VAR1 :: Hi $VAR2"
+# echo "$VAR2 :: Hello ${VAR1}"
+
+
+VAR1=tom
+VAR2=jerry
 
 echo "$VAR1 :: Hi $VAR2"
-echo "$VAR2 :: Hello ${VAR1}"
+echo "$VAR2 :: Hello $VAR1"
+echo "$VAR1 :: How are you doing"
+echo "$VAR2 :: Great" 
