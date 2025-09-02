dcb53c7afd8c575c63116b18f4aae14e13b17a7a
sajja surya <sajja.rao@zeiss.com>
2025-09-02 22:14:20 +0530

    files backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -41,8 +41,6 @@ fi
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-echo $0
-
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
 if [ -n "$FILES" ] # true if there are files to zip
