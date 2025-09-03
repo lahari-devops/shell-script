ecf9f752de248e43c808da9ddfca36d2cefb5b72
sajja surya <sajja.rao@zeiss.com>
2025-09-03 17:21:00 +0530

    disk usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -2,9 +2,17 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5    # Real projects will monitor for 70% or 80%
+MSG=""
+
 while read -r line
 do 
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "usage : $USAGE and partition : $PARTITION"
+    # echo "usage : $USAGE and partition : $PARTITION"
+    if [ $PARTITION -ge $DISK_THRESHOLD ]
+    then 
+        $MSG+="High disk usage on partion: $PARTITION with usage: $USAGE"
+    fi
 done <<< $DISK_USAGE
+
+echo -e "Messege : $MSG"
