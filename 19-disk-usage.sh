165c26542d087245865bb73be0425661d97e9b53
sajja surya <sajja.rao@zeiss.com>
2025-09-03 17:13:44 +0530

    disk usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5    # Real projects will monitor for 70% or 80%
 while read -r line
 do 
-    UAGAE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
     echo "usage : $USAGE and partition : $PARTITION"
 done <<< $DISK_USAGE
